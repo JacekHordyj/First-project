62816133cd712fc9d0ad837aea9d38380b484cba
JacekHordyj <114860442+JacekHordyj@users.noreply.github.com>
2022-10-03 21:19:49 +0200

    final pull commit

--- other/views.py
+++ other/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import  HttpResponse
 
 def simple_view(request):
-    return HttpResponse('HELLO! ignore rules added, another pull')
+    return HttpResponse('HELLO! ignore rules added, another pull,final pull')
 
 # Create your views here.
