52194f84ec4777505ac3ed3a5eeb61a4497435a6
JacekHordyj <114860442+JacekHordyj@users.noreply.github.com>
2022-10-03 21:10:51 +0200

    confirmin ignore works
    adding ignore

--- other/views.py
+++ other/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import  HttpResponse
 
 def simple_view(request):
-    return HttpResponse('HELLO!')
+    return HttpResponse('HELLO! ignore rules added')
 
 # Create your views here.
